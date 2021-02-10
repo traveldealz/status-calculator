0471eb87432f4eb1053fc904ee516013272b3be0
Johannes Kinast <johannes@kinast.me>
2021-02-10 16:39:40 +0000

    deploy Wed Feb 10 2021 16:39:39 GMT+0000 (Coordinated Universal Time)

--- dist/index.js
+++ dist/index.js
@@ -794,7 +794,7 @@ function calculateSegments$3(segments) {
 var CM = {
   name: 'Copa Airlines ConnectMiles',
   alliance: 'Star Alliance',
-  qualificationPeriodType: 'Calendar Year',
+  qualificationPeriodType: 'Calendar year',
   status: [{
     name: 'Silver',
     allianceStatus: 'Star Alliance Silver',
@@ -1375,8 +1375,38 @@ var programs = {
   AC
 };
 
-const template = document.createElement("template");
-template.innerHTML =
+function translate (text, trans = []) {
+  return text.replace(/__\((.+?)\)/g, (match, group) => {
+    return trans[group] ? trans[group] : group;
+  });
+}
+
+var trans_de = {
+  "Calculate": "Berechnen",
+  "Data provided by": "Daten bereitgestellt von",
+  "miles": "Meilen",
+  "segment": "Segment",
+  "segments": "Segmente",
+  "month": "Monat",
+  "months": "Monate",
+  "Validity": "Gültigkeit",
+  "Qualification period": "Qualifikationszeitraum",
+  "Calendar year": "Kalenderjahr",
+  "Membership months": "Mitgliedsmonate",
+  "Membership year": "Mitgliedsjahr",
+  "Consecutive months": "aufeinanderfolgende Monate",
+  "at least": "mind.",
+  "of": "von"
+};
+
+var trans_es = {};
+
+const translations = {
+  en: [],
+  de: trans_de,
+  es: trans_es
+};
+const template =
 /*html*/
 `
   <style>
@@ -1385,61 +1415,44 @@ template.innerHTML =
   }
   </style>
   <form>
-    <textarea name="route">LH:A:FRA-HKG</textarea><br />
-    <select name="status">
-      <option>Star Alliance Silver</option>
-      <option selected>Star Alliance Gold</option>
-    </select>
-    <button type="submit">Calculate</button>
+    <label for="route">__(Routings):</label>
+    <textarea name="route" class="w-full my-1">LH:A:FRA-HKG-MUC</textarea>
+    <small></small>
+    <div class="my-3">
+      <label for="status">__(Status)</label>
+      <select name="status">
+        <option>Star Alliance Silver</option>
+        <option selected>Star Alliance Gold</option>
+      </select>
+    </div>
+    <button class="px-3 py-1 bg-brand hover:bg-gray-darker text-white" type="submit">__(Calculate)</button>
   </form>
   <ol id="list" data-columns="3"></ol>
-  <p><small>Data provided by <a href="https://www.wheretocredit.com">wheretocredit.com</a></small></p>
-`; // const needed = {
-//   A3: 72000,
-//   UA: 50000,
-//   OZ: 40000,
-//   TK: 40000,
-//   SK: 45000,
-//   LHM: 100000,
-//   SQ: 50000,
-//   ET: 50000,
-//   CA: 80000,
-//   MS: 60000,
-//   TG: 50000,
-//   NZ: 900,
-//   CM: 45000,
-//   AC: 50000,
-//   NH: 50000,
-//   AV: 40000,
-//   BR: 50000,
-//   SA: 60000,
-//   TP: 70000,
-//   AI: 50000,
-// };
+  <p><small>__(Data provided by) <a href="https://www.wheretocredit.com" target="_blank">wheretocredit.com</a></small></p>
+`;
 
 class StatusCalculator extends HTMLElement {
   constructor() {
     super();
     this.count = 0;
-    this.attachShadow({
-      mode: "open"
-    });
     this.$status = 'Star Alliance Gold';
     this.$segments = [];
   }
 
   connectedCallback() {
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
-    this.el_route = this.shadowRoot.querySelector('[name="route"]');
-    this.el_list = this.shadowRoot.querySelector('#list');
-    this.shadowRoot.querySelector('form').addEventListener('submit', event => {
+    this.$locale = this.hasAttribute('locale') ? this.getAttribute('locale') : navigator.language ? navigator.language : 'en';
+    this.$locale = this.$locale.split('-')[0];
+    this.innerHTML = translate(template, translations[this.$locale] ? translations[this.$locale] : []);
+    this.el_route = this.querySelector('[name="route"]');
+    this.el_list = this.querySelector('#list');
+    this.querySelector('form').addEventListener('submit', event => {
       event.preventDefault();
       this.calculate();
     });
   }
 
   calculate() {
-    this.$status = this.shadowRoot.querySelector('[name="status"]').value;
+    this.$status = this.querySelector('[name="status"]').value;
     let itineraries = this.el_route.value.trim().split('\n').map(value => {
       let parts = value.split(':').map(v => v.trim());
       let carrier = parts[0];
@@ -1543,32 +1556,48 @@ class StatusCalculator extends HTMLElement {
       }).flat().filter(status => undefined !== typeof status.progress);
     }).flat().sort((a, b) => b.progress - a.progress).forEach(item => {
       let el = document.createElement('li');
-      el.innerHTML = `
-        <strong>${item.program.name}: ${item.status.name}</strong><br />
-        <small>${item.progress.toLocaleString(undefined, {
+      let text = `
+        <h3>${item.program.name}: ${item.status.name}</h3>
+        <div class="grid grid-cols-2 gap-4 my-3" style="gap: .75rem;">
+          <div class="${'undefined' === typeof item.secProgress ? 'col-span-2' : ''}">
+            <div class="text-sm">${item.progress.toLocaleString(undefined, {
         style: 'percent',
         minimumFractionDigits: 0
-      })} = ${item.collected.toLocaleString()} of ${item.needed.toLocaleString()} ${item.qualification.type}</small><br />
-        <progress value="${item.progress}">${item.progress.toLocaleString(undefined, {
+      })} = ${item.collected.toLocaleString()} __(of) ${item.needed.toLocaleString()} __(${item.qualification.type})</div>
+            <progress class="w-full" value="${item.progress}">${item.progress.toLocaleString(undefined, {
         style: 'percent',
         minimumFractionDigits: 0
       })}</progress>
+          </div>
         ${'undefined' === typeof item.secProgress ? '' : `
-          <br /><small>${item.secProgress.toLocaleString(undefined, {
+          <div>
+            <div class="text-sm">${item.secProgress.toLocaleString(undefined, {
         style: 'percent',
         minimumFractionDigits: 0
-      })} = ${item.secCollected.toLocaleString()} of ${item.secNeeded.toLocaleString()} ${item.qualification.secType}</small><br />
-          <progress value="${item.secProgress}">${item.secProgress.toLocaleString(undefined, {
+      })} = ${item.secCollected.toLocaleString()} __(of) ${item.secNeeded.toLocaleString()} __(${item.qualification.secType})</div>
+            <progress class="w-full" value="${item.secProgress}">${item.secProgress.toLocaleString(undefined, {
         style: 'percent',
         minimumFractionDigits: 0
       })}</progress>
+          </div>
           `}
-        ${item.qualification.note?.en ? `<br /><small>${item.qualification.note.en}</small>` : ''}
-        ${item.status.note?.en ? `<br /><small>${item.status.note.en}</small>` : ''}
-        ${item.program.note?.en ? `<br /><small>${item.program.note.en}</small>` : ''}
-        <br /><small>Qualification period: ${item.qualification.qualificationPeriod} month (${item.program.qualificationPeriodType})</small>
-        <br /><small>Validity: at least ${item.qualification.validity} months</small>
+          <div class="col-span-2 text-sm">
+            ${item.qualification.note && item.qualification.note[this.$locale] ? `<div>${item.qualification.note[this.$locale]}</div>` : ''}
+            ${item.status.note && item.status.note[this.$locale] ? `<div>${item.status.note[this.$locale]}</div>` : ''}
+            ${item.program.note && item.program.note[this.$locale] ? `<div>${item.program.note[this.$locale]}</div>` : ''}
+          </div>
+          <div class="text-center">
+            <div class="text-sm">__(Qualification period)</div>
+            <div>${item.qualification.qualificationPeriod} __(months)</div>
+            <div class="text-sm">__(${item.program.qualificationPeriodType})</div>
+          </div>
+          <div class="text-center">
+            <div class="text-sm">__(Validity)</div>
+            <div>__(at least) ${item.qualification.validity} __(months)</div>
+          </div>
+        </div>
         `;
+      el.innerHTML = translate(text, translations[this.$locale] ? translations[this.$locale] : []);
       this.el_list.appendChild(el);
     });
   }
