3ffc5eae1e360ee689a36c026116a14889d71fb4
Johannes Kinast <johannes@kinast.me>
2021-02-26 09:47:12 +0000

    deploy Fri Feb 26 2021 09:47:11 GMT+0000 (Coordinated Universal Time)

--- dist/index.js
+++ dist/index.js
@@ -24,6 +24,8 @@ var trans_de = {
   "Routings": "Strecken",
   "Calculate": "Berechnen",
   "Data provided by": "Daten bereitgestellt von",
+  "Award Miles Data provided by": "Prämienmeilen-Daten bereitgestellt von",
+  "Points Data provided by Travel-Dealz.eu": "Punkte-Daten bereitgestellt von Travel-Dealz.de",
   "miles": "Meilen",
   "segment": "Segment",
   "segments": "Segmente",
@@ -42,7 +44,12 @@ var trans_de = {
   "Airline": "Fluggesellschaft",
   "Booking Class": "Buchungsklasse",
   "Points": "Punkte",
-  "Total": "Gesamt"
+  "Total": "Gesamt",
+  "Distance": "Entfernung",
+  "miles": "Meilen",
+  "From": "Von",
+  "To": "Nach",
+  "Award Miles": "Prämienmeilen"
 };
 
 var trans_es = {};
@@ -2029,6 +2036,16 @@ var template$2 = /*html*/
   button[disabled] {
     background-color: gray;
   }
+  .align-top {
+    vertical-align: top;
+  }
+  .text-vertical {
+    writing-mode: vertical-rl;
+    text-orientation: sideways
+  }
+  .font-light {
+    font-weight: 300;
+  }
   </style>
   <form>
     <label for="route">__(Routings)</label>
@@ -2037,11 +2054,15 @@ BA:W:LHR-HKG</textarea>
     <small></small>
     <div class="my-3">
       <button class="mr-3 px-3 py-1 bg-brand hover:bg-gray-darker text-white" type="submit">__(Calculate)</button>
+      <label for="status">__(Status)</label>
+      <select name="status">
+      </select>
     </div>
   </form>
   <div class="loading hidden">__(Loading & calculating...)</div>
   <div class="error hidden"></div>
   <table id="list"></table>
+  <p><small>__(Points Data provided by Travel-Dealz.eu) & __(Award Miles Data provided by) <a href="https://www.wheretocredit.com" target="_blank">wheretocredit.com</a></small></p>
 `;
 
 class TierpointsCalculator extends BaseComponent {
@@ -2054,14 +2075,24 @@ class TierpointsCalculator extends BaseComponent {
   connectedCallback() {
     super.connectedCallback();
     this.$program = this.hasAttribute('program') ? this.getAttribute('program') : 'BA';
+    this.$points_label = this.hasAttribute('points_label') ? this.getAttribute('points_label') : null;
+    this.$awardmiles_label = this.hasAttribute('awardmiles_label') ? this.getAttribute('awardmiles_label') : null;
+    this.$status_labels = this.hasAttribute('status_labels') ? this.getAttribute('status_labels').split(',') : ['None', 'Silver', 'Gold', 'Platinum'];
+    this.el_status = this.querySelector('[name="status"]');
+    this.$status_labels.forEach((status, index) => {
+      let el_option = document.createElement('option');
+      el_option.value = index;
+      el_option.innerHTML = status;
+      this.el_status.appendChild(el_option);
+    });
   }
 
   calculate() {
     super.calculate();
   }
 
-  query(itineraries) {
-    fetch('https://farecollection.travel-dealz.de/api/calculate/tierpoints', {
+  async query(itineraries) {
+    Promise.all([fetch('https://farecollection.travel-dealz.de/api/calculate/tierpoints', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -2071,23 +2102,42 @@ class TierpointsCalculator extends BaseComponent {
           segments: [itinerary]
         };
       }))
-    }).then(response => response.json()).then(data => this.display(data.value)).catch(error => {
+    }), fetch('https://www.wheretocredit.com/api/2.0/calculate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(itineraries.map(itinerary => {
+        return {
+          segments: [itinerary]
+        };
+      }))
+    })]).then(responses => Promise.all(responses.map(response => response.json()))).then(responses => this.display(responses[0], responses[1])).catch(error => {
       this.loading_end();
       this.el_error.innerHTML = `Travel Dealz Tier Points Calculator ${error.toString()}`;
       this.el_error.classList.remove('hidden');
     });
   }
 
-  display(data) {
+  display({
+    value: data,
+    airlines,
+    airports
+  }, {
+    value: wtc_data
+  }) {
     super.display(data);
     this.el_list.innerHTML = '';
     let el_thead = document.createElement('thead');
-    el_thead.innerHTML = translate(`
+    el_thead.innerHTML = translate(
+    /*html*/
+    `
       <tr>
+        <th></th>
         <th class="text-center">__(Route)</th>
-        <th class="text-center">__(Airline)</th>
-        <th class="text-center">__(Booking Class)</th>
-        <th class="text-right">__(Points)</th>
+        <th class="text-center">__(Bookingclass)</th>
+        <th class="text-right">${this.$awardmiles_label ? this.$awardmiles_label : '__(Award Miles)'}</th>
+        <th class="text-right">${this.$points_label ? this.$points_label : '__(Points)'}</th>
       </tr>
     `, translations[this.$locale] ? translations[this.$locale] : []);
     this.el_list.appendChild(el_thead);
@@ -2097,22 +2147,58 @@ class TierpointsCalculator extends BaseComponent {
       });
       return totals;
     }, {});
+    const status_key = this.el_status.value;
+    let totals_rdm = wtc_data.reduce((totals, itinerary) => {
+      itinerary.value.totals.forEach(item => {
+        totals[item.id] = totals[item.id] ? totals[item.id] + item.rdm[status_key] : item.rdm[status_key];
+      });
+      return totals;
+    }, {});
     this.$segments.forEach((segment, index) => {
+      const earning = data[index].value.totals.find(item => item.id === this.$program);
+      const earning_rdm = wtc_data[index].value.totals.find(item => item.id === this.$program);
       let el = document.createElement('tr');
-      el.innerHTML = `
-        <td class="text-center"><code>${segment.origin}</code> - <code>${segment.destination}</code></td>
-        <td class="text-center"><code>${segment.carrier}</code></td>
-        <td class="text-center"><code>${segment.bookingClass}</code></td>
-        <td class="text-right">${false === data[index].success ? data[index].errorMessage : `${data[index].value.totals.find(item => item.id === this.$program) ? data[index].value.totals.find(item => item.id === this.$program).qm[0] : 0}`}</td>
-        `;
+      el.innerHTML = translate(
+      /*html*/
+      `
+        <td class="align-top text-vertical text-center text-xs text-grey-dark font-light">${data[index].value.distance?.toLocaleString()} __(miles)</td>
+        <td class="text-center">
+          <div class="grid grid-cols-1 md:grid-cols-2">
+            <div>
+              <div><code>${segment.origin}</code></div>
+              <div class="text-xs text-grey-dark font-light">${airports[segment.origin]?.location}</div>
+            </div>
+            <div>
+              <div><code>${segment.destination}</code></div>
+              <div class="text-xs text-grey-dark font-light">${airports[segment.destination]?.location}</div>
+            </div>
+          </div>
+        </td>
+        <td class="text-center">
+          <div class="grid grid-cols-1 md:grid-cols-2">
+            <div>
+              <div><code>${segment.carrier}</code></div>
+              <div class="text-xs text-grey-dark font-light">${airlines[segment.carrier]?.name}</div>
+            </div>
+            <div>
+              <div><code>${segment.bookingClass}</code></div>
+              <div class="text-xs text-grey-dark font-light">${earning.cabinclass} ${earning.fare}</div>
+            </div>
+          </div>
+        </td>
+        <td class="text-right">${false === wtc_data[index].success ? wtc_data[index].errorMessage : `${earning_rdm ? earning_rdm.rdm[status_key]?.toLocaleString() : 0}`}</td>
+        <td class="text-right">${false === data[index].success ? data[index].errorMessage : `${earning ? earning.qm[0]?.toLocaleString() : 0}`}</td>
+        `, translations[this.$locale] ? translations[this.$locale] : []);
       this.el_list.appendChild(el);
     });
     let el_foot = document.createElement('tfoot');
-    console.log(this.$program);
-    el_foot.innerHTML = translate(`
+    el_foot.innerHTML = translate(
+    /*html*/
+    `
       <tr>
         <th class="text-right" colspan="3">__(Total)</th>
-        <th class="text-right">${totals[this.$program].toLocaleString()}</th>
+        <th class="text-right">${totals_rdm[this.$program]?.toLocaleString()}</th>
+        <th class="text-right">${totals[this.$program]?.toLocaleString()}</th>
       </tr>
     `, translations[this.$locale] ? translations[this.$locale] : []);
     this.el_list.appendChild(el_foot);
