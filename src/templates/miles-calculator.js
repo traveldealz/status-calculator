5ad2073854279febe2933c5a283721d283cd5235
diddyhsv <ditmarlange@hotmail.de>
2021-08-05 14:48:28 +0000

    Kleineres Feld und TD in small

--- src/templates/miles-calculator.js
+++ src/templates/miles-calculator.js
@@ -11,7 +11,7 @@ export default /*html*/ `
   </style>
   <form>
     <label for="route">__(Routings)</label>
-    <textarea name="route" class="w-full my-1" rows="8">LH:A:FRA-HKG-MUC</textarea>
+    <textarea name="route" class="w-full my-1" rows="2">LH:A:FRA-HKG-MUC</textarea>
     <small></small>
     <div class="my-3">
       <button class="mr-3 px-3 py-1 bg-brand hover:bg-gray-darker text-white" type="submit">__(Calculate)</button>
@@ -20,5 +20,5 @@ export default /*html*/ `
   <div class="loading hidden">__(Loading & calculating...)</div>
   <div class="error hidden"></div>
   <ul id="list"></ul>
-  <p><small>__(Data provided by) <a href="https://www.wheretocredit.com" target="_blank">wheretocredit.com</a></small> and Travel-Dealz.de</p>
+  <p><small>__(Data provided by) <a href="https://www.wheretocredit.com" target="_blank">wheretocredit.com</a> and Travel-Dealz.de</small> </p>
 `;
