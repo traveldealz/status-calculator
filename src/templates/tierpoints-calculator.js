fbad9a9223ab171febeff064409d2db2386c5d70
diddyhsv <ditmarlange@hotmail.de>
2021-09-29 13:34:14 +0000

    Individual program calculator

--- src/templates/tierpoints-calculator.js
+++ src/templates/tierpoints-calculator.js
@@ -16,18 +16,21 @@ export default /*html*/ `
   </style>
   <form>
     <label for="route">__(Routings)</label>
-    <textarea name="route" class="w-full my-1" rows="3">BA:K:FRA-LHR
-BA:W:LHR-HKG</textarea>
+    <textarea id="route" name="route" class="w-full my-1" rows="3">AF:L:FRA-AMS-FRA
+AF:X:ATL-CDG-ATL</textarea>
     <small></small>
     <div class="my-3">
-      <button class="mr-3 px-3 py-1 bg-brand hover:bg-gray-darker text-white" type="submit">__(Calculate)</button>
-      <label for="status">__(Status)</label>
-      <select name="status">
+      <label for="program">__(Program)</label>
+      <select id="program" name="program">
       </select>
+      <label for="status">__(Current status)</label>
+      <select id="status" name="status">
+      </select>
+      <button class="mr-3 px-3 py-1 bg-brand hover:bg-gray-darker text-white" type="submit">__(Calculate)</button>
     </div>
   </form>
   <div class="loading hidden">__(Loading & calculating...)</div>
   <div class="error hidden"></div>
-  <table id="list"></table>
+  <table id="list" name="list"></table>
   <p><small>__(Data provided by Travel-Dealz.eu)</small></p>
 `;
