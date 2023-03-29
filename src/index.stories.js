561cbb1a7809e28f7fe7e07f01bb49befac36cca
Johannes Kinast <5927337+undefined@users.noreply.github.com>
2023-03-29 10:48:13 +0000

    Add Distance Calculator

--- src/index.stories.js
+++ src/index.stories.js
@@ -11,3 +11,5 @@ export const TierPointsCalcualtor = () =>
   `<tierpoints-calculator></tierpoints-calculator>`;
 export const MilesCalculator = () =>
   `<miles-calculator route="SK:T:TLL-CPH-PVG-CPH-TLL"></miles-calculator>`;
+export const DistanceCalculator = () =>
+  `<distance-calculator></distance-calculator>`;
