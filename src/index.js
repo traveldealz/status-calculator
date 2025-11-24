7e26fc25f8290a91266e034b4f7257b8fe185d38
Johannes Kinast <johannes@kinast.me>
2025-11-24 16:47:38 +0100

    Register RouteBuilder first

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import MilesCalculator from "./miles-calculator-component.js";
 import DistanceCalculator from "./distance-calculator-component.js";
 import RouteBuilder from "./route-builder-component.js";
 
+customElements.define("route-builder", RouteBuilder);
 customElements.define("status-calculator", StatusCalculator);
 customElements.define("tierpoints-calculator", TierpointsCalculator);
 customElements.define("ua-pqp-calculator", UaPqpCalculator);
 customElements.define("miles-calculator", MilesCalculator);
 customElements.define("distance-calculator", DistanceCalculator);
-customElements.define("route-builder", RouteBuilder);
